2b338657badfe1e2d95bdec84fbcc4949e5248ca
AntonPlish <antonplish@gmail.com>
2022-06-10 10:47:38 +0300

    Commit Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
+const palletteContainer = document.querySelector('.gallery');
 
-// console.log(galleryItems);
+palletteContainer.insertAdjacentHTML('beforeend', createCardsGallery(galleryItems))
+
+function createCardsGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div><a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a></div>`;
+        })
+        .join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionClass: '.gallery__image',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    captionsData: 'alt',
+});
+
+console.log(galleryItems);
